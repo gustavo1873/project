bd059f783018bb91a73d621226fa4e2ef136a8d7
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-28 03:29:21 -0300

    switch animation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
+/* Se quiser que a imagem tambem mude quando alternar entre light/dark mode
+   
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/2500.webp")
@@ -9,6 +10,7 @@ function toggleMode() {
   } else {
     img.setAttribute("src", "./assets/maxresdefault.jpg")
   }
+  */
 }
 /* alternativa para o toggle
 if (html.classList.contains("light")) {
