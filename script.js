16d31486270cbf78015407ef839287602097b825
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-28 02:48:29 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/2500.webp")
+    img.setAttribute("alt", "Gato de botas imponente")
+  } else {
+    img.setAttribute("src", "./assets/maxresdefault.jpg")
+  }
+}
+/* alternativa para o toggle
+if (html.classList.contains("light")) {
+  html.classList.remove("light")
+} else {
+  html.classList.add("light")
+}*/
